1d69ce7c783e9be989be02c78f57098530424333
alex <alexanderyi933@gmail.com>
2024-07-28 23:52:22 -0400

    initial commit in spring boot project

---
+++ src/main/java/com/jar_api/jar_api/JarApiApplication.java
@@ -0,0 +1,14 @@
+package com.jar_api.jar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world spring");
+		SpringApplication.run(JarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jar_api/jar_api/JarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jar_api.jar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
